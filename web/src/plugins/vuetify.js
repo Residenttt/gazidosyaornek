e9bc6fc59b2f308a6c741969425fe7c351764ee5
alpidev <alpidev9@gmail.com>
2020-04-20 18:20:24 +0300

    night mode

--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -1,11 +1,25 @@
 import '@mdi/font/css/materialdesignicons.min.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import colors from 'vuetify/lib/util/colors'
 import i18n from '../i18n'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+	theme: {
+		themes: {
+			light: {
+				primary: colors.purple,
+				secondary: colors.grey.darken1,
+				accent: colors.shades.black,
+				error: colors.red.accent3,
+			},
+			dark: {
+				primary: colors.blue.lighten3,
+			},
+		},
+	},
 	icons: {
 		iconfont: 'mdi'
 	},
